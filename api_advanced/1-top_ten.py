5905c12105dccd44c83248eed6f2a84b8a211392
root <s.ihimbazwe@alustdudent.com>
2025-02-20 14:03:18 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
-def top_ten(subreddit):
-    """Print titles of first 10 hot posts for a given subreddit."""
-    
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    }
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 404:
-            print("None")
-            return
-            
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            
-            if not posts:
-                print("None")
-                return
-                
-            for post in posts[:10]:
-                print(post.get('data', {}).get('title', ''))
-        else:
-            print("None")
-            
-    except Exception:
-        print("None")
 
-if __name__ == '__main__':
-    # Test cases
-    print("Testing with 'python' subreddit:")
-    top_ten('python')
-    
-    print("\nTesting with non-existent subreddit:")
-    top_ten('this_subreddit_does_not_exist')
+def top_ten(subreddit):
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
+        print(None)
